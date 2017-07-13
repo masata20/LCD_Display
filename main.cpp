13071ff2da71ba19ccaeeb8d832f0fd44d93a5b0
Masataka Mori <masataka.mori20@gmail.com>
2017-07-13 18:46:17 +0900

    add row, colunm, total_rows, total_colunm, vairables

--- main.cpp
+++ main.cpp
@@ -10,13 +10,26 @@ int main()
 	
 	while (scanf("%d%d", &bar_size, &num), bar_size)
 	{
-		printf("bar_size is %d, num is %d\n", bar_size, num);
+		//printf("bar_size is %d, num is %d\n", bar_size, num);
 
+		// determine the number of digits in input number
 		int digits = countDigits(num);
 		
-		printf("digits is : %d\n", digits);
+		//printf("digits is : %d\n", digits);
+		
+		// caluculate row and colunm for one digit
+		int row = 2*bar_size + 3;
+		int colunm = bar_size + 2;
 
+		printf("row is %d\n", row);
+		printf("colunm is %d\n", colunm);
 
+		// determine total rows and total colunms to make 2 dimentional array
+		int total_rows = row; // same as the row for each digits
+		int total_colunms = (digits*colunm) + (digits - 1);
+
+		printf("total_rows is %d\n", total_rows);
+		printf("total_colunms is %d\n", total_colunms);
 
 	}
 
