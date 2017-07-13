519e3dde2eab002980b4485f590fa30386f340ec
Masataka Mori <masataka.mori20@gmail.com>
2017-07-13 19:19:13 +0900

    add 2d array initalize with all 0s

--- main.cpp
+++ main.cpp
@@ -21,15 +21,39 @@ int main()
 		int row = 2*bar_size + 3;
 		int colunm = bar_size + 2;
 
-		printf("row is %d\n", row);
-		printf("colunm is %d\n", colunm);
+	//	printf("row is %d\n", row);
+	//	printf("colunm is %d\n", colunm);
 
 		// determine total rows and total colunms to make 2 dimentional array
 		int total_rows = row; // same as the row for each digits
 		int total_colunms = (digits*colunm) + (digits - 1);
 
-		printf("total_rows is %d\n", total_rows);
-		printf("total_colunms is %d\n", total_colunms);
+	//	printf("total_rows is %d\n", total_rows);
+	//	printf("total_colunms is %d\n", total_colunms);
+	
+		// declare 2 dimentional array all elements are zero
+		// 0 : display bar
+		// 1 : don't display bar
+		int lcd_number[total_rows][total_colunms];
+
+		// Init with zeros, maybe another better ways		
+		for (int i = 0; i < total_rows; i++)
+		{
+			for (int j = 0; j < total_colunms; j++)
+			{
+				lcd_number[i][j] = 0;
+			}
+		}
+       /*	
+		for (int i = 0; i < total_rows; i++)
+		{
+			for (int j = 0; j < total_colunms; j++)
+			{
+				printf("%d", lcd_number[i][j]);
+			}
+			printf("\n");
+		}
+		*/
 
 	}
 
