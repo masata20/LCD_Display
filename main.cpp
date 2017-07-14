c05b311bd1b5325401c011c767a486a6df46278d
Masataka Mori <masataka.mori20@gmail.com>
2017-07-14 17:48:49 +0900

    added algorithm to change the digit '6' to lcd format in 2d array

--- main.cpp
+++ main.cpp
@@ -277,6 +277,26 @@ void lcdConverter(int bar_size, int digitNumber, int raw_value, int** arr)
 
 	if (raw_value == 6)
 	{
+        // for rows
+		for (int i = 1; i <= bar_size; i++)
+		{
+			// first row
+			arr[row_begin][colunm_begin+i] = 2;
+			// middle row
+			arr[row_middle][colunm_begin+i] = 2;
+			// end row
+			arr[row_end][colunm_begin+i] = 2;
+		}
+     	// for first colunm and ene colunm
+		for (int i = 1; i <= bar_size; i++)
+		{
+	    	// first half
+			arr[row_begin+i][colunm_begin] = 1;
+			
+			// second half
+			arr[row_end-i][colunm_begin] = 1;
+			arr[row_end-i][colunm_end] = 1;
+		}
 
 		return;
 	}
